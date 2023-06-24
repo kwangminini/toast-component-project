79f5ee98e5824e99e7104d5e79d4872bce62c98b
kwangminini <rhkdals7362@gmail.com>
2023-06-24 16:42:50 +0900

    feat: add Toaster Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Toaster } from "~/components/common/ToastNotification";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Toaster>
+      <App />
+    </Toaster>
   </React.StrictMode>
 );
