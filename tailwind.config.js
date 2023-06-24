076425d753b2a8d14862863866dcd1ac128e20e0
kwangminini <rhkdals7362@gmail.com>
2023-06-25 00:12:19 +0900

    feat: add toast custom backgroundColor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "toast-bg": "#ffffff",
+        "toast-success-bg": "#1e8ffa",
+      },
+    },
   },
   plugins: [],
 };
