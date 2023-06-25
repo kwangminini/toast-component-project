305389f38b8931639b69c33b0754acfab3701a7d
kwangminini <rhkdals7362@gmail.com>
2023-06-25 17:56:05 +0900

    fix: 'x' is not rendering

--- src/components/common/Button/CloseButton.tsx
+++ src/components/common/Button/CloseButton.tsx
@@ -3,10 +3,10 @@ import { IButtonProps } from "~/components/common/Button/types";
 export function CloseButton({ className, onClick }: IButtonProps) {
   return (
     <button
-      className={`w-6 after:content-["\\00d7"] after:text-xl ${
-        className ?? ""
-      }`}
+      className={`w-6 flex items-center justify-center ${className ?? ""}`}
       onClick={onClick}
-    ></button>
+    >
+      x
+    </button>
   );
 }
