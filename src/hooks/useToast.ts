080ad3aed76ed148a181639c7be342333832c230
kwangminini <rhkdals7362@gmail.com>
2023-06-26 00:00:42 +0900

    refactor: delete unnecessary logic

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,11 +1,7 @@
-import { IToastParams } from "~/components/common/Toast/types";
 import { useToastContext } from "~/hooks/useToastContext";
 
 export default function useToast() {
   const { addToast } = useToastContext();
 
-  const toast = ({ variant, message, duration, render }: IToastParams) => {
-    addToast({ variant, message, duration, render });
-  };
-  return toast;
+  return addToast;
 }
